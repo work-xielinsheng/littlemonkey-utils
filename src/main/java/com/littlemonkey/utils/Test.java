7192cfa076e868c64a7ad2739e1be4960b31f2c3
minmx <Zhangmengqin2018>
2018-06-07 01:18:47 +0800

    修复http连接工具代码

--- src/main/java/com/littlemonkey/utils/Test.java
+++ src/main/java/com/littlemonkey/utils/Test.java
@@ -2,29 +2,34 @@ package com.littlemonkey.utils;
 
 import com.littlemonkey.utils.http.HttpConnections;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Test {
 
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(100);
-        for (int i = 0; i < 100; i++) {
-            int cc = (int) Math.round(99999999 * Math.random());
-            String url = "http://localhost:8080/api/test/" + cc;
-            System.out.println(System.currentTimeMillis());
-            System.out.println(url);
-            System.out.println(i);
-            executorService.execute(new Runnable() {
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newFixedThreadPool(1000);
+        List<Callable<String>> list = new ArrayList<Callable<String>>();
+        for (int i = 0; i < 1000; i++) {
+            list.add(new Callable<String>() {
                 @Override
-                public void run() {
+                public String call() throws Exception {
+                    System.out.println(System.currentTimeMillis());
+                    int cc = (int) Math.round(99999999 * Math.random());
+                    String url = "http://localhost:8080/api/test/" + cc;
                     HttpConnections.createHttpConnection(null);
                     String content = HttpConnections.GET(url, null, null);
                     if (!content.equals(String.valueOf(cc))) {
                         throw new RuntimeException("?????????????????????????????????");
                     }
+                    return null;
                 }
             });
         }
+        System.out.println("???");
+        executorService.invokeAll(list);
     }
 }
